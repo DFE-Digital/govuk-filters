bacd28bafac1be5882dc1eb45fb77c538267b33d
Joseph Hull <josephhull676@yahoo.co.uk>
2023-01-19 16:16:10 +0000

    Joe temp

--- app/components/filters_component.rb
+++ app/components/filters_component.rb
@@ -8,9 +8,5 @@ class FiltersComponent < ViewComponent::Base
     super
   end
 
-  def container_class
-    container.sidebar ? "govuk-grid-column-one-third" : "govuk-grid-column-full"
-  end
-
   renders_one :container, FiltersComponent::ContainerComponent
 end
